b85e9bd81df25c1ed56d108a2f86bb863d39a9b1
jf-t <jackftilly@gmail.com>
2019-01-13 15:00:06 -0500

    more formatting for add-event page

--- ui/src/components/add-event/add-event.jsx
+++ ui/src/components/add-event/add-event.jsx
@@ -2,19 +2,25 @@ import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 
+import { createEvent } from '../../actions/eventActions';
+
 import './add-event.scss';
 
 class AddEventComponent extends Component {
   constructor (props) {
     super(props);
 
+    this.state = {
+      name: '',
+      startDate: '',
+      endDate: '',
+      calendarId: null
+    };
+
     this.createEvent = this.createEvent.bind(this);
+    this.chooseCalendar = this.chooseCalendar.bind(this);
     this.changeInput = this.changeInput.bind(this);
     this.displayCalendars = this.displayCalendars.bind(this);
-  }
-
-  createEvent () {
-    console.log("create event send");
   }
 
   changeInput (name) {
@@ -23,18 +29,37 @@ class AddEventComponent extends Component {
     }
   }
 
+  chooseCalendar (id) {
+    this.setState({ calendarId: id });
+  }
+
   displayCalendars () {
+    // Filter will work based on calendar ID from backend
     return (
       <div>
         <h1>Choose a Calendar</h1>
-        <h2>Work Calendar</h2>
-        <h2>Family Calendar</h2>
-        <h2>Personal Calendar</h2>
+        <h2><a onClick={() => this.chooseCalendar(1)}>Work Calendar</a></h2>
+        <h2><a onClick={() => this.chooseCalendar(2)}>Family Calendar</a></h2>
+        <h2><a onClick={() => this.chooseCalendar(3)}>Personal Calendar</a></h2>
       </div>
     );
   }
 
+  createEvent (e) {
+    e.preventDefault();
+
+    console.log("createEvent");
+    console.log(this.state);
+
+    this.props.createEvent(this.state);
+  }
+
   render () {
+    if (this.props.eventInfo) {
+      this.props.history.push('/');
+    }
+
+    // Breadcrumbs can be in components util
     return (
       <div className="add-event-page">
         <div className="breadcrumbs">
@@ -42,11 +67,18 @@ class AddEventComponent extends Component {
         </div>
 
         <form onSubmit={this.createEvent}>
-          <input type="text" onChange={this.changeInput('name')} placeholder="Event Name"></input>
+          <input type="text"
+                 onChange={this.changeInput('name')}
+                 placeholder="Event Name"></input>
 
           <div className="flex">
-            <input type="text" onChange={this.changeInput('startDate')} placeholder="Start Date"></input>
-            <input type="text" onChange={this.changeInput('endDate')} placeholder="End Date"></input>
+            <input type="text"
+                   onChange={this.changeInput('startDate')}
+                   placeholder="Start Date"></input>
+
+            <input type="text"
+                   onChange={this.changeInput('endDate')}
+                   placeholder="End Date"></input>
           </div>
 
           <div className="calendars">
@@ -54,6 +86,10 @@ class AddEventComponent extends Component {
           </div>
 
           <textarea onChange={this.changeInput('description')}></textarea>
+
+          <div className="create-button">
+            <input type="submit" value="Create" />
+          </div>
         </form>
       </div>
     );
@@ -61,12 +97,13 @@ class AddEventComponent extends Component {
 }
 
 const mapStateToProps = state => ({
-
+  eventInfo: state.event.activeEvent,
+  error: state.event.error
 });
 
 const mapDispatchToProps = dispatch => ({
-
+  createEvent: (event) => dispatch(createEvent(event))
 });
 
 
-export default connect () (AddEventComponent);
+export default connect (mapStateToProps, mapDispatchToProps) (AddEventComponent);
